90f03bb71d9bcf0e0f7717e7679be96727a8adb5
Vikas Beniwal <beniwalv@google.com>
2023-09-27 11:43:24 +0000

    first commit for wine Statstics

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import StatisticsTable from "./StatisticsTable";
+import wineData from "./wineData.json"; // Replace with your data source
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Wine Data Statistics</h1>
+      <StatisticsTable data={wineData} property="Flavanoids" />
     </div>
   );
 }
---
+++ src/StatisticsTable.js
@@ -0,0 +1,140 @@
+import React from 'react';
+import { calculateMean, calculateMedian, calculateMode,calculateGamma } from './utils';
+
+const StatisticsTable = ({ data, property }) => {
+  // console.log(data, 'data')
+  const classList = [...new Set(data.map((wine) => wine.Alcohol))];
+  const gammaProperties = data.map((item)=>calculateGamma(item))
+  const newData = data.map((item, index)=>{return{...item , "gamma":gammaProperties[index]}})
+  //  console.log(newData, 'gammaProperties11');
+  const getPropertyValues = (property, classNumber) => {
+    console.log(property, 'pppppppp')
+    if(property==="Flavanoids"){
+      return calculateMean(
+        data
+          .filter((wine) => wine.Alcohol === classNumber)
+          .map((wine) => wine[property])
+      ).toFixed(3);
+    }if(property==="gamma"){
+      const filtered = newData
+      .filter((wine) => wine.Alcohol === classNumber)
+      console.log(filtered, 'filter')
+      console.log(filtered.map((wine)=>wine[property]));
+
+      return calculateMean(
+        newData
+          .filter((wine) => wine.Alcohol === classNumber)
+          .map((wine) => wine[property])
+      ).toFixed(3);
+    }
+    
+  };
+
+  const getPropertyMedians = (property, classNumber) => {
+    if(property==="Flavanoids"){
+      return calculateMedian(
+        data
+          .filter((wine) => wine.Alcohol === classNumber)
+          .map((wine) => wine[property])
+      ).toFixed(3);
+
+    }
+    if(property==="gamma"){
+      return calculateMedian(
+        newData
+          .filter((wine) => wine.Alcohol === classNumber)
+          .map((wine) => wine[property])
+      ).toFixed(3);
+      
+    }
+
+  };
+
+  const getPropertyModes = (property, classNumber) => {
+    if(property==="Flavanoids"){
+      return calculateMode(
+        data
+          .filter((wine) => wine.Alcohol === classNumber)
+          .map((wine) => wine[property])
+      ).toFixed(3);;
+
+    }
+    if(property==="gamma"){
+      return calculateMode(
+        newData
+          .filter((wine) => wine.Alcohol === classNumber)
+          .map((wine) => wine[property])
+      ).toFixed(3);;
+    }
+    
+  };
+
+  return (
+    <div className='container'>
+      <h1>Wine Statistics</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Measure</th>
+            {classList.map((classNumber) => (
+              <th key={classNumber}>Class {classNumber}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{property} Mean</td>
+            {classList.map((classNumber) => (
+              <td key={classNumber}>{getPropertyValues(property, classNumber)}</td>
+            ))}
+          </tr>
+          <tr>
+            <td>{property} Median</td>
+            {classList.map((classNumber) => (
+              <td key={classNumber}>{getPropertyMedians(property, classNumber)}</td>
+            ))}
+          </tr>
+          <tr>
+            <td>{property} Mode</td>
+            {classList.map((classNumber) => (
+              <td key={classNumber}>{getPropertyModes(property, classNumber)}</td>
+            ))}
+          </tr>
+        </tbody>
+      </table>
+      <h1>Gamma Statistics</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Measure</th>
+            {classList.map((classNumber) => (
+              <th key={classNumber}>Class {classNumber}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Gamma Mean</td>
+            {classList.map((classNumber) => (
+              <td key={classNumber}>{getPropertyValues("gamma", classNumber)}</td>
+            ))}
+          </tr>
+          <tr>
+            <td> Gamma Median</td>
+            {classList.map((classNumber) => (
+              <td key={classNumber}>{getPropertyMedians("gamma", classNumber)}</td>
+            ))}
+          </tr>
+          <tr>
+            <td>Gamma Mode</td>
+            {classList.map((classNumber) => (
+              <td key={classNumber}>{getPropertyModes("gamma", classNumber)}</td>
+            ))}
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default StatisticsTable;
